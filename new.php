ea065a1b1b935f3218ea9e5decc147f3c511e06a
PrestonMclean <mclean_preston@hotmail.com>
2021-02-08 00:13:03 -0600

    remove edit and modify new.php
    removed edit.php and added functionality to new.php

--- new.php
+++ new.php
@@ -1,49 +1,79 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>MyJournal</title>
-        <link href="https://fonts.googleapis.com/css?family=Cousine:400" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Work+Sans:600" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/site.css">
-    </head>
-    <body>
-        <header>
-            <div class="container">
-                <div class="site-header">
-                    <a class="logo" href="index.php"><i class="material-icons">library_books</i></a>
-                    <a class="button icon-right" href="new.php"><span>New Entry</span> <i class="material-icons">add</i></a>
-                </div>
-            </div>
-        </header>
-        <section>
-            <div class="container">
+<?php
+include('inc/header.php');
+
+list($id, $title, $date, $time_spent, $learned, $resources) = get_entry(filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT));
+
+if ($id) {
+$tags = get_tags($id);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+    $title = trim(filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING));
+    $date = trim(filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING));
+    $time_spent = filter_input(INPUT_POST, 'timeSpent', FILTER_SANITIZE_STRING);
+    $learned = filter_input(INPUT_POST, 'whatILearned', FILTER_SANITIZE_STRING);
+    $resources = filter_input(INPUT_POST, 'ResourcesToRemember', FILTER_SANITIZE_STRING);
+    $tags = null;
+    foreach (get_all_tags() as $tag) {
+      if (!empty($_POST[$tag['id']])) {
+        $tags[] = $_POST[$tag['id']];
+      }
+    }
+
+    if (empty($title) || empty($date) || empty($time_spent) || empty($learned) || empty($resources) || empty($tags)) {
+        $error_message = 'Please make sure to fill in the required fields: Title, Date, Time spent, What I Learned, Resources to Remember, and Tags';
+    } else {
+        if (add_entry($title, $date, $time_spent, $learned, $resources, $tags, $id)) {
+            header('Location: index.php');
+            exit;
+        } else {
+            $error_message = 'Could not add project';
+        }
+    }
+}
+
+?>
                 <div class="new-entry">
-                    <h2>New Entry</h2>
-                    <form>
+                  <?php if(!empty($error_message)) { echo "<h2>". $error_message ."</h2>";}?>
+                    <h2><?php
+                      if($id) {
+                        echo "Edit ";
+                      } else {
+                        echo "New ";
+                      }
+                    ?>Entry</h2>
+                    <form method="post" action="<?php echo 'new.php?id=' . $id;?>">
                         <label for="title"> Title</label>
-                        <input id="title" type="text" name="title"><br>
+                        <input id="title" type="text" name="title" value="<?php echo $title;?>"><br>
                         <label for="date">Date</label>
-                        <input id="date" type="date" name="date"><br>
+                        <input id="date" type="date" name="date" value="<?php echo $date;?>"><br>
                         <label for="time-spent"> Time Spent</label>
-                        <input id="time-spent" type="text" name="timeSpent"><br>
+                        <input id="time-spent" type="text" name="timeSpent" value="<?php echo $time_spent;?>"><br>
                         <label for="what-i-learned">What I Learned</label>
-                        <textarea id="what-i-learned" rows="5" name="whatILearned"></textarea>
+                        <textarea id="what-i-learned" rows="5" name="whatILearned"><?php echo $learned;?></textarea>
                         <label for="resources-to-remember">Resources to Remember</label>
-                        <textarea id="resources-to-remember" rows="5" name="ResourcesToRemember"></textarea>
+                        <textarea id="resources-to-remember" rows="5" name="ResourcesToRemember"><?php echo $resources;?></textarea>
+                        <?php
+                          foreach (get_all_tags() as $tag) {
+                            if (!empty($tags) && in_array($tag['tag'], $tags)) {
+                              foreach ($tags as $item) {
+                                if ($tag['tag'] == $item) {
+                                  echo "<input id='".$tag['id']."' type='checkbox' name='".$tag['id']."' value='".$tag['tag']."' checked> ";
+                                  echo "<label for='".$tag['tag']."' class='tag'>  ". $tag['tag'] ."</label></br>";
+                                  $item = null;
+                                }
+                              }
+                            } else {
+                              echo "<input id='".$tag['id']."' type='checkbox' name='".$tag['id']."' value='".$tag['tag']."'> ";
+                              echo "<label for='".$tag['tag']."' class='tag'> ". $tag['tag'] ."</label></br>";
+                            }
+                          }
+                        ?>
                         <input type="submit" value="Publish Entry" class="button">
-                        <a href="#" class="button button-secondary">Cancel</a>
+                        <a href="index.php" class="button button-secondary">Cancel</a>
                     </form>
                 </div>
-            </div>
-        </section>
-        <footer>
-            <div>
-                &copy; MyJournal
-            </div>
-        </footer>
-    </body>
-</html>
+<?php
+  include('inc/footer.php');
+?>
