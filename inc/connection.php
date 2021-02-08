3066abf49b29502b51beef111db210e0fc25b570
PrestonMclean <mclean_preston@hotmail.com>
2021-02-08 00:17:43 -0600

    add connection
    add the connection to the database

--- inc/connection.php
+++ inc/connection.php
@@ -5,10 +5,7 @@
   try {
     $db = new PDO('sqlite:./inc/journal.db');
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $results = $db->query('SELECT * FROM entries');
-    echo "<pre>";
-    var_dump($results->fetchAll());
-    echo "</pre>";
   } catch (Exception $e) {
     echo 'Error!: ' . $e->getMessage();
+    die();
   }
