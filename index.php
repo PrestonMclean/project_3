dc97303b762f64f2f6c8164ee2e067eed906d85c
PrestonMclean <mclean_preston@hotmail.com>
2021-02-08 00:16:27 -0600

    update index
    show entries from newest to oldest

--- index.php
+++ index.php
@@ -1,56 +1,50 @@
 <?php
+include('inc/header.php');
 
-  require_once('inc/connection.php');
+if(isset($_POST['delete'])) {
+  if (delete_entry($_POST['delete'])) {
+    header("Location: index.php?msg=Entry+Deleted");
+    exit;
+  } else {
+    $error_message = 'could not delete that entry';
+  }
+}
 
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>MyJournal</title>
-        <link href="https://fonts.googleapis.com/css?family=Cousine:400" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Work+Sans:600" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/site.css">
-    </head>
-    <body>
-        <header>
-            <div class="container">
-                <div class="site-header">
-                    <a class="logo" href="index.php"><i class="material-icons">library_books</i></a>
-                    <a class="button icon-right" href="new.php"><span>New Entry</span> <i class="material-icons">add</i></a>
-                </div>
-            </div>
-        </header>
-        <section>
-            <div class="container">
                 <div class="entry-list">
-                    <article>
-                        <h2><a href="detail.php">The best day I’ve ever had</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_2.php">The absolute worst day I’ve ever had</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_3.php">That time at the mall</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_4.php">Dude, where’s my car?</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
+                  <?php
+                    if (!empty($_GET['tag'])) {
+                      $entries = get_entries(get_tags_ids($_GET['tag']));
+                      //var_dump(get_entries(2));
+                    } else {
+                      $entries = get_entries();
+                    }
+                    foreach ($entries as $entry) {
+                      echo "<article>\n";
+                          echo "<h2>";
+                          echo "<a href='detail.php?id=" . $entry['id'] . "'>" . $entry['title'] . "</a>";
+                          echo "</h2>\n";
+                          echo "<ul id='tag_list'>";
+                          foreach (get_tags($entry['id']) as $tag) {
+                            echo "<li><a href='index.php?tag=". $tag ."'>" . ($tag) . "</a></li>";
+                          }
+                          echo "</ul>";
+                          echo "<form method=post action='index.php' onsubmit=\"return confirm('Are you sure you want tot delete this project?');\">\n";
+                          echo "<input type='hidden' value='" . $entry['id'] . "' name='delete' />\n";
+                          echo "<input type='submit' class='button--delete' value='Delete' />\n";
+                          echo "</form>\n";
+                          echo "<time datetime='". date( 'Y-m-d' , strtotime($entry['date'])) ."'>";
+                             echo date( 'F j, Y' , strtotime($entry['date']));
+                          echo "</time>\n";
+                      echo "</article>\n";
+                    }
+                  ?>
+                  <article>
+                    <h2>
+                      <a href="new.php">+ add new entry</a>
+                    </h2>
+                  </article>
                 </div>
-            </div>
-        </section>
-        <footer>
-            <div>
-                &copy; MyJournal
-            </div>
-        </footer>
-    </body>
-</html>
+<?php
+  include('inc/footer.php');
+?>
